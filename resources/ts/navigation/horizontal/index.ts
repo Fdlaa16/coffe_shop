fb552a3eab0f21d82b79bfc34522f6ec6d3d6ef4
pajri <pajri@sismedika.com>
2025-07-16 08:04:17 +0700

    fix setup

--- resources/ts/navigation/horizontal/index.ts
+++ resources/ts/navigation/horizontal/index.ts
@@ -1,13 +1,13 @@
+import type { HorizontalNavItems } from '@layouts/types'
 import apps from './apps'
 import charts from './charts'
 import dashboard from './dashboard'
 import forms from './forms'
+import master from './master'
 import misc from './misc'
+import order from './order'
 import pages from './pages'
 import tables from './tables'
 import uiElements from './ui-elements'
-import type { HorizontalNavItems } from '@layouts/types'
-import master from './master'
-import order from './order'
 
-export default [...dashboard, ...master, ...order. ...apps, ...pages, ...uiElements, ...forms, ...tables, ...charts, ...misc] as HorizontalNavItems
+export default [...dashboard, ...master, ...order, ...apps, ...pages, ...uiElements, ...forms, ...tables, ...charts, ...misc] as HorizontalNavItems
